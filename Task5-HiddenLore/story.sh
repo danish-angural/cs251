f3e60a813e8902bf70100f2a2d0b80905f96afa5
KanPard005 <kanad5501@gmail.com>
2020-08-16 21:09:31 +0530

    Added tasks 3 and 5

---
+++ Task5-HiddenLore/story.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+key=$2 
+while true 
+	do { 
+		k=false
+		for file in $1/*
+		do {
+				if [[ $key = $(head -2 $file | tail -1) ]]
+				then  {
+					k=true
+					echo $(head -1 $file | tail -1) >> $3
+					key=$(head -3 $file | tail -1)
+					break
+				}
+				fi
+			}
+		done
+		if [[ $k = false ]] 
+		then exit 0
+		fi
+	}
+done
+				
