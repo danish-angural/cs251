ba978937598406a85f3761344af9069efeb48d0e
KanPard005 <kanad5501@gmail.com>
2020-08-18 21:30:12 +0530

    Removed unnecessary parts from roman.sh

--- Task1-Integers/roman.sh
+++ Task1-Integers/roman.sh
@@ -3,14 +3,12 @@ a="100.C 90.XC 50.L 40.XL 10.X 9.IX 5.V 4.IV 1.I"
 n=$1
 s=""
 while [[ $n -gt 0 ]]
-do {
+do 
 	for v in $a
-		do {
-			if [[ ${v%.*} -lt n ]] || [[ ${v%.*} -eq n ]] 
+		do 
+			if [[ ${v%.*} -le n ]] 
 			then { s+=${v#*.}; (( n-=${v%.*} )); break; }
 			fi
-		}
 	done
-}
 done	
 echo "$s";
