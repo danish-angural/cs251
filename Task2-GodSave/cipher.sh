a12605ef744df963b672d4e0875538c7dea97b1d
KanPard005 <kanad5501@gmail.com>
2020-08-19 18:50:26 +0530

    Adding final line to encrypted.txt

--- Task2-GodSave/cipher.sh
+++ Task2-GodSave/cipher.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 url=$1
 if [ "$url" == "" ] 
-then echo 'Usage: ./cipher.txt <url>'
+then echo 'Usage: ./cipher.sh <url>'
 exit 1
 fi
 
 wget -q "$url"
-set=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
-set2=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
-l=$(tail -n 1 ./letter.txt)
+mv $(basename $url) encrypted.txt
+set=aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ
+l=$(tail -n 1 ./encrypted.txt)
 for i in {1..26}
 do
-l=$(echo "$l" | tr  "${set:0:26}" "${set:1:27}")
-l=$(echo "$l" | tr  "${set2:0:26}" "${set2:1:27}")
+l=$(echo "$l" | tr  "${set:0:52}" "${set:2:54}")
 if echo "$l" | grep -q -i -e "Queen" -e "Majesty" -e "Mary"
 then
 break
 fi
 done
-tr "${set:0:26}" "${set:${i}:26+${i}}" < ./letter.txt > decrypted.txt && mv decrypted.txt letter.txt
-tr "${set2:0:26}" "${set2:${i}:26+${i}}" <letter.txt >deciphered.txt
+(( i*=2 ))
+tr "${set:0:52}" "${set:${i}:52+${i}}" < encrypted.txt > deciphered.txt
+
 
 ans="PS. Give me the names."
-ans=$(echo "$ans" | tr  "${set:${i}:26+${i}}" "${set:0:26}")
-ans=$(echo "$ans" | tr  "${set2:${i}:26+${i}}" "${set2:0:26}")
-echo "$ans" > "./encrypted.txt"
-rm letter.txt
+ans=$(echo "$ans" | tr  "${set:${i}:52+${i}}" "${set:0:52}")
+echo "$ans" >> "./encrypted.txt"
