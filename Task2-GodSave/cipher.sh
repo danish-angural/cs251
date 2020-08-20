a145c4411310e1aeb1f3d273e4cf78c3b95666f2
KanPard005 <kanad5501@gmail.com>
2020-08-21 00:57:25 +0530

    Removed check for Mary

--- Task2-GodSave/cipher.sh
+++ Task2-GodSave/cipher.sh
@@ -8,11 +8,11 @@ fi
 wget -q "$url"
 mv $(basename $url) encrypted.txt
 set=aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ
-l=$(tail -n 1 ./encrypted.txt)
+l=$(tail -n 2 ./encrypted.txt)
 for i in {1..26}
 do
 l=$(echo "$l" | tr  "${set:0:52}" "${set:2:54}")
-if echo "$l" | grep -q -i -e "Queen" -e "Majesty" -e "Mary"
+if echo "$l" | grep -q -i -e "Queen" -e "Majesty"
 then
 break
 fi
