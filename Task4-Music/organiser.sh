e6bee07abcb224d25c59eb037352088cd1ead562
danish-angural <62121923+danish-angural@users.noreply.github.com>
2020-08-20 07:51:18 +0530

    Update organiser.sh

--- Task4-Music/organiser.sh
+++ Task4-Music/organiser.sh
@@ -10,7 +10,6 @@ num_playlists=$(cat "$f" | head -n 2 | tail -1)
 for ((i=3; i<num_playlists+3; i++))
 do playlist_name=$(cat "$f" | head -n "$i" | tail -1)
 [ -e "./playlists/$playlist_name" ] || mkdir "./playlists/$playlist_name"
-t=$(basename "$f")
 [ -e  ./playlists/"$playlist_name/$t" ] || ln -s "$(realpath "$f")" ./playlists/"$playlist_name/$t"
 done
 done
